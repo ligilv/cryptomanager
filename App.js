6d1b7b52d525e0234c39203fa5a9c3dda3bf710b
Ligil V James <ligil@Ligils-MacBook-Air.local>
2022-05-23 00:55:16 +0530

    check status of app

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import {NavigationContainer} from '@react-navigation/native';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import FlashMessage from 'react-native-flash-message';
 import { store } from './src/redux/store';
 import { Provider } from 'react-redux';
@@ -18,14 +18,33 @@ import {
   StyleSheet,
   Text,
   useColorScheme,
-  View,
+  View,AppState
 } from 'react-native';
 import Home from './src/screens/Home';
 import RootNavigator from './src/navigation';
 import { storage } from './src/utils/mmkvStorage';
 const App = () => {
+  const appState = useRef(AppState.currentState);
   const username = storage.getString('user.name')
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
   console.log("check",username); 
+  useEffect(()=>{
+    const subscription = AppState.addEventListener("change", nextAppState => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === "active"
+      ) {
+        console.log("App has come to the foreground!");
+      }
+
+      appState.current = nextAppState;
+      setAppStateVisible(appState.current);
+  console.log("AppState", appState.current);
+    })
+return () => {
+  subscription.remove();
+};
+}, [])
   return (
     <Provider store={store}>
     <NavigationContainer>
