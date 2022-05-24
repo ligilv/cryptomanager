1188d331dc0b2c961ffb8eba8aa7992f5623b769
Ligil V James <ligil@Ligils-MacBook-Air.local>
2022-05-25 00:26:59 +0530

    reevert

--- src/screens/Favorites/index.js
+++ src/screens/Favorites/index.js
@@ -13,20 +13,11 @@ const Favorites = () => {
   const data = [1, 2];
   return (
     <SafeAreaView style={{flex: 1}}>
-      {/* <Text>Favorites</Text>
- <Button title='logout' onPress={logout}/>  */}
+      <Text>Favorites</Text>
+ <Button title='logout' onPress={logout}/> 
 
-      <FlatList
-        data={data}
-        renderItem={({item}) => {
-          return (
-            <Image
-              source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
-              style={{height: 200, width: '30%'}}
-            />
-          );
-        }}
-      />
+ 
+
     </SafeAreaView>
   );
 };
